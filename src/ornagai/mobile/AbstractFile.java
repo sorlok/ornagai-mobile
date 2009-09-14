e91ec76a3066dbafd97f35a01988025df0939eee
seth.hetu <seth.hetu@6322c550-9ab2-11de-9965-3f0b11712531>
2009-09-14 22:45:03 +0000

    Loading a dictionary's word list and lookup table works! Now all that's left is the list of definitions.
    Later, we might have to put the word's definition index (bitid) into the bitstream, but for now we're just concerned with looking up words.

--- src/ornagai/mobile/AbstractFile.java
+++ src/ornagai/mobile/AbstractFile.java
@@ -6,11 +6,12 @@ import java.io.InputStream;
  * @author Seth N. Hetu
  */
 public abstract class AbstractFile {
-    protected abstract InputStream getFileAsInputStream();
+    protected abstract InputStream getFileAsInputStream(String resourceName);
+    public abstract boolean exists(String resourceName);
     protected abstract void closeFile();
 
-    public void openProcessClose(ProcessAction p) {
-        p.processFile(getFileAsInputStream());
+    public void openProcessClose(String resourceName, ProcessAction p) {
+        p.processFile(getFileAsInputStream(resourceName));
         closeFile();
     }
 }
