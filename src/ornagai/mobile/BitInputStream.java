393eb992efcd6f1c6bc6d8557b6dc6bef4845d3c
seth.hetu <seth.hetu@6322c550-9ab2-11de-9965-3f0b11712531>
2009-09-14 06:36:46 +0000

    Fixed some bugs in the creation of binary files. Now, the wordlist loads properly, yay!

---
+++ src/ornagai/mobile/BitInputStream.java
@@ -0,0 +1,68 @@
+package ornagai.mobile;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class BitInputStream
+{
+  private InputStream in;
+  private byte[] buffer = new byte[1024];
+  private int count;
+  
+  private int nextBit = 0;
+  private int currByte = 0;
+
+  //Assume we'll be reading to the end of the file
+  public BitInputStream( InputStream in ) {
+    this.in = in;
+    try {
+        count = in.read(buffer);
+    } catch (IOException ex) {
+        throw new RuntimeException("Can't read bit: " + ex.toString());
+    }
+  }
+
+  synchronized public int readBit() throws IOException {
+    if (in == null)
+      throw new IOException( "Already closed" );
+
+    //Free to read?
+    if (nextBit==8) {
+        currByte++;
+        nextBit = 0;
+        if (currByte>=count) {
+            //Read more bytes
+            currByte = 0;
+            try {
+                count = in.read(buffer);
+            } catch (IOException ex) {
+                throw new RuntimeException("Can't read bit: " + ex.toString());
+            }
+        }
+        //System.out.println("On byte: " + Integer.toHexString((0xFF&buffer[currByte])));
+    }
+
+    int match = buffer[currByte] & (1<<(7-nextBit++));
+    return match == 0 ? 0 : 1;
+  }
+
+  //Slow, but at least it works for now.
+  synchronized public int readNumber(int bits) throws IOException {
+      int value = 0;
+      while (bits>0) {
+          int bit = readBit();
+          //System.out.println(" bit: " + bit);
+          value <<= 1;
+          value |= bit;
+          bits--;
+      }
+      return value;
+  }
+
+  public void close() throws IOException {
+    in.close();
+    in = null;
+  }
+}
+
