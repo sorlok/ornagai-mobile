743290a5e4ab6c8a13e617a005a600886ade07a3
seth.hetu <seth.hetu@6322c550-9ab2-11de-9965-3f0b11712531>
2009-09-25 05:23:40 +0000

    Beginnings of a FileChooser API.

---
+++ src/ornagai/mobile/filebrowser/FileChooser.java
@@ -0,0 +1,19 @@
+package ornagai.mobile.filebrowser;
+
+import com.sun.lwuit.Form;
+import com.sun.lwuit.Image;
+
+/**
+ *
+ * @author Seth N. Hetu
+ */
+public class FileChooser {
+
+    //Simple call and return, with cancel and ok commands
+    //  Returns null if nothing was selected. Else, returns the path of the selected item.
+    public static String browseForFile(Form previousPage, String[] fileSuffixes, Image[] fileIcons, Image folderIcon) {
+        
+    }
+
+
+}
