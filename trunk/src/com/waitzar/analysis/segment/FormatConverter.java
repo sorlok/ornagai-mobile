b3cc1cdfe738d93a0d6bb2183d68ed0b1a64ecf5
seth.hetu <seth.hetu@6322c550-9ab2-11de-9965-3f0b11712531>
2009-10-05 08:57:57 +0000

    Added in framework for conversion, letter tagging. Need to add combinational logic.

--- trunk/src/com/waitzar/analysis/segment/FormatConverter.java
+++ trunk/src/com/waitzar/analysis/segment/FormatConverter.java
@@ -8,6 +8,8 @@ package com.waitzar.analysis.segment;
 //NOTE: This converter was intended to be FAST and mild on memory, not necessarily accurate.
 //      It was tested and exhibited decent accuracy for most strings; however, a full
 //      round of spot-checking is necessary before we can remove the experimental label.
+//NOTE: Passing in Zawgyi-2008 strings should not cause any problems; the converter was
+//      designed to output roughly the same string.
 //NOTE: It is best to use the segmentation algorithm (WZSegment) to re-order a few particularly
 //      ugly strings of text.
 public class FormatConverter {
@@ -221,6 +223,314 @@ public class FormatConverter {
     }
 
 
+    private static final int GetIdentifier(char letter) {
+        switch (letter) {
+            //ID_VOWEL_A
+            case 0x1031:
+                return ID_VOWELL_A;
+
+
+            //ID_YA_YIT
+            case 0x103B:
+            case 0x107E:
+            case 0x107F:
+            case 0x1080:
+            case 0x1081:
+            case 0x1082:
+            case 0x1083:
+            case 0x1084:
+                return ID_YA_YIT;
+
+
+            //ID_CONSONANT
+            // Includes things we're treating as consonants (numbers, complex)
+            // (we are trying to give reasonable results on a string already
+            //  in Zawgyi 2008 format)
+            case 0x1000:
+            case 0x1001:
+            case 0x1002:
+            case 0x1003:
+            case 0x1004:
+            case 0x1005:
+            case 0x1006:
+            case 0x1007:
+            case 0x1008:
+            case 0x1009:
+            case 0x100A:
+            case 0x100B:
+            case 0x100C:
+            case 0x100D:
+            case 0x100E:
+            case 0x100F:
+            case 0x1010:
+            case 0x1011:
+            case 0x1012:
+            case 0x1013:
+            case 0x1014:
+            case 0x1015:
+            case 0x1016:
+            case 0x1017:
+            case 0x1018:
+            case 0x1019:
+            case 0x101A:
+            case 0x101B:
+            case 0x101C:
+            case 0x101D:
+            case 0x101E:
+            case 0x101F:
+            case 0x1020:
+            case 0x1021:
+          //case 0x1022:
+            case 0x1023:
+            case 0x1024:
+            case 0x1025:
+            case 0x1026:
+            case 0x1027:
+            case 0x1028:
+            case 0x1029:
+            case 0x1040:
+            case 0x1041:
+            case 0x1042:
+            case 0x1043:
+            case 0x1044:
+            case 0x1045:
+            case 0x1046:
+            case 0x1047:
+            case 0x1048:
+            case 0x1049:
+            case 0x104A:
+            case 0x104B:
+            case 0x104C:
+            case 0x104D:
+            case 0x104E:
+            case 0x104F:
+            case 0x106A:
+            case 0x106B:
+            case 0x106E:
+            case 0x106F:
+            case 0x1086:
+            case 0x108F:
+            case 0x1090:
+            case 0x1091:
+            case 0x1092:
+            case 0x1097:
+                return ID_CONSONANT;
+
+
+            //ID_STACKED_CONSONANT
+            case 0x1060:
+            case 0x1061:
+            case 0x1062:
+            case 0x1063:
+            //case 0x1064: //not kinzi
+            case 0x1065:
+            case 0x1066:
+            case 0x1067:
+            case 0x1068:
+            case 0x1069:
+            case 0x106C:
+            case 0x106D:
+            case 0x1070:
+            case 0x1071:
+            case 0x1072:
+            case 0x1073:
+            case 0x1074:
+            case 0x1075:
+            case 0x1076:
+            case 0x1077:
+            case 0x1078:
+            case 0x1079:
+            case 0x107A:
+            case 0x107B:
+            case 0x107C:
+            case 0x1085:
+            case 0x1096:
+                return ID_STACKED_CONSONANT;
+
+
+            //ID_KINZI
+            case 0x1064:
+            case 0x108B:
+            case 0x108C:
+            case 0x108D:
+                return ID_KINZI;
+
+
+            //ID_YA_PIN
+            case 0x103A:
+            case 0x107D:
+                return ID_YA_PIN;
+
+
+            //ID_CIRCLE_BELOW
+            case 0x103C:
+            case 0x108A:
+                return ID_CIRCLE_BELOW;
+
+
+            //ID_LEG_BACK
+            case 0x103D:
+            case 0x1087:
+            case 0x1088:
+            case 0x1089:
+                return ID_LEG_BACK;
+
+
+            //ID_CIRCLE_ABOVE
+            case 0x102D:
+            case 0x108E:
+                return ID_CIRCLE_ABOVE;
+
+
+            //ID_CIRCLE_ABOVE_CROSSED
+            case 0x102E:
+                return ID_CIRCLE_ABOVE_CROSSED;
+
+
+            //ID_SLASH_ABOVE
+            case 0x1032:
+                return ID_SLASH_ABOVE;
+
+
+            //ID_DOT_ABOVE
+            case 0x1036:
+                return ID_DOT_ABOVE;
+
+
+            //ID_AR_TALL
+            case 0x102B:
+            case 0x105A:
+                return ID_AR_TALL;
+
+
+            //ID_AR_SHORT
+            case 0x102C:
+                return ID_AR_SHORT;
+
+
+            //ID_LEG_FORWARD
+            case 0x102F:
+            case 0x1033:
+                return ID_LEG_FORWARD;
+
+
+            //ID_DOUBLE_LEG_FORWARD
+            case 0x1030:
+            case 0x1034:
+                return ID_DOUBLE_LEG_FORWARD;
+
+
+            //ID_ASAT
+            case 0x1039:
+                return ID_ASAT;
+
+
+            //ID_VISARGA
+            case 0x1038:
+                return ID_VISARGA;
+
+
+            //ID_DOT_BELOW
+            case 0x1037:
+            case 0x1094:
+            case 0x1095:
+                return ID_DOT_BELOW;
+
+
+            //Everything else
+            default:
+                return ID_NOID;
+        }
+    }
+
+    
+    //Get any additional data for Zawgyi 2008's combinational letters.
+    private static final int GetSupplementaryIdentifier(int primmaryID, char letter) {
+        //We don't technically have to switch on the primary ID, but I want to 
+        //  enforce that this should only be called AFTEr GetIdentifier() is called.
+        switch (primmaryID) {
+            case ID_STACKED_CONSONANT:
+                if (letter==0x1069)
+                    return ID_YA_PIN;
+                break;
+            case ID_KINZI:
+                if (letter==0x108B)
+                    return ID_CIRCLE_ABOVE;
+                else if (letter==0x108C)
+                    return ID_CIRCLE_ABOVE_CROSSED;
+                else if (letter==0x108D)
+                    return ID_DOT_ABOVE;
+                break;
+            case ID_CIRCLE_BELOW:
+                if (letter==0x108A)
+                    return ID_LEG_BACK;
+                break;
+            case ID_LEG_BACK:
+                if (letter==0x1088)
+                    return ID_LEG_FORWARD;
+                else if (letter==0x1089)
+                    return ID_DOUBLE_LEG_FORWARD;
+                break;
+            case ID_CIRCLE_ABOVE:
+                if (letter==0x108E)
+                    return ID_DOT_ABOVE;
+                break;
+            case ID_AR_TALL:
+                if (letter==0x105A)
+                    return ID_ASAT;
+                break;
+        }
+        
+        return ID_NOID;
+    }
+
+
+
+    private static final void CombineAndReduce(String[] identifiers, int currID) {
+        //The main part of our algorithm. What to combine, and when, and why.
+        switch (currID) {
+            case ID_VOWELL_A:
+                break;
+            case ID_YA_YIT:
+                break;
+            case ID_CONSONANT:
+                break;
+            case ID_STACKED_CONSONANT:
+                break;
+            case ID_KINZI:
+                break;
+            case ID_YA_PIN:
+                break;
+            case ID_CIRCLE_BELOW:
+                break;
+            case ID_LEG_BACK:
+                break;
+            case ID_CIRCLE_ABOVE:
+                break;
+            case ID_CIRCLE_ABOVE_CROSSED:
+                break;
+            case ID_SLASH_ABOVE:
+                break;
+            case ID_DOT_ABOVE:
+                break;
+            case ID_AR_TALL:
+                break;
+            case ID_AR_SHORT:
+                break;
+            case ID_LEG_FORWARD:
+                break;
+            case ID_DOUBLE_LEG_FORWARD:
+                break;
+            case ID_ASAT:
+                break;
+            case ID_VISARGA:
+                break;
+            case ID_DOT_BELOW:
+                break;
+        }
+    }
+
+
     private static final String ScanTrickyReplacements(String source) {
         //Our algorithm is fairly simple. It operates on strings of partial syllables, scanning
         //  until it finds a consonant OR the vowel "ay" OR the medial "ya yit" (the only
@@ -250,10 +560,79 @@ public class FormatConverter {
         //Apologies for the long comment, but I'd like to give hackers some chance at understanding
         //  why we chose to lex the source this way.
         String[] identifiers = new String[ID_TOTALIDENTIFIERS];
+        StringBuffer res = new StringBuffer();
+        for (int startID=0; startID<source.length();startID++) {
+            //First, advance startID past any non-Myanmar letters
+            if (source.charAt(startID)<0x1000 || source.charAt(startID)>0x109F)
+                continue;
+
+            //Reset identifiers
+            for (int i=0; i<identifiers.length; i++)
+                identifiers[i] = null;
+
+            //Next, scan until the first prefix vowel or consonant (base or killed is ok)
+            // Meanwhile, set all entries in the identifiers array
+            int endID = startID;
+            for (;endID<source.length();endID++) {
+                char nextChar = source.charAt(endID);
+                int nextID = GetIdentifier(nextChar);
+                if (nextID==ID_NOID)
+                    continue;
+
+                //Time to quit, if we've already passed a consonant
+                if (nextID==ID_CONSONANT || nextID==ID_VOWELL_A || nextID==ID_YA_YIT) {
+                    if (identifiers[ID_CONSONANT]!=null)
+                        break;
+                }
+
+                //Fake quit case: exit early if we'd be losing information
+                if (identifiers[nextID]!=null) {
+                    System.out.println("Warning: identifier already matched: " + nextID);
+                    break;
+                }
+
+                //Normal case: just store it
+                identifiers[nextID] = ""+nextChar;
+
+                //Special cases for text already in Zawgyi 2008
+                int suppID = GetSupplementaryIdentifier(nextID, nextChar);
+                if (suppID!=ID_NOID)
+                    identifiers[suppID] = "";
+            }
+
+            //Catch looping forever:
+            if (startID==endID)
+                throw new RuntimeException("Format Conversion algorithm exited in error.");
+
+            //Now, scan through the same string again
+            for (int i=startID; i<=endID; i++) {
+                char currChar = source.charAt(i);
+                int currID = GetIdentifier(currChar);
+                if (currID==ID_NOID) {
+                    //Append unknown characters. Possibly unwise, but I want to avoid
+                    //  silent errors for the "testing" phase
+                    res.append(currChar);
+                } else {
+                    //Has this already been processed
+                    if (identifiers[currID]==null || identifiers[currID].length()==0)
+                        continue;
+
+                    //Otherwise, handle the main conversion process
+                    CombineAndReduce(identifiers, currID);
+
+                    //We are guaranteed that our identifiers array will have at least
+                    //  the empty string after this process
+                    res.append(identifiers[currID]);
+                }
+            }
+
+            //Increment
+            startID = endID;
+        }
 
 
 
-        return source; //For now...
+        return res.toString();
     }
 
 }
