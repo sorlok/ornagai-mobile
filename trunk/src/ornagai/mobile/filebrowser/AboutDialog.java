0c7a04af7e7b09fe21e1ef075909289471bf7d37
seth.hetu <seth.hetu@6322c550-9ab2-11de-9965-3f0b11712531>
2009-10-19 00:31:14 +0000

    "About" box is done.

--- trunk/src/ornagai/mobile/filebrowser/AboutDialog.java
+++ trunk/src/ornagai/mobile/filebrowser/AboutDialog.java
@@ -7,6 +7,7 @@ import com.sun.lwuit.events.ActionListener;
 import com.sun.lwuit.layouts.*;
 import com.sun.lwuit.plaf.*;
 import java.util.Vector;
+import ornagai.mobile.MZMobileDictionary;
 import ornagai.mobile.RoundButton;
 import ornagai.mobile.gui.FormController;
 
@@ -50,8 +51,43 @@ public class AboutDialog {
         aboutForm.getStyle().setBgColor(0x000000);
         aboutForm.setTitle("About Ornagai Mobile");
 
-        //aboutForm.addComponent(new Label("About..."));
+        //Main body label
+        TextArea aboutLbl = new TextArea();
+        aboutLbl.getStyle().setBgTransparency(0);
+        aboutLbl.getStyle().setBorder(Border.createEmpty());
+        aboutLbl.setRows(1);
+        aboutLbl.setMaxSize(Integer.MAX_VALUE);
+        aboutLbl.setEditable(false);
+        aboutLbl.setFocusable(false);
+        aboutLbl.setText("Version 2.0\nNotable features:\n  * 30,000 words\n  * Small & fast\n  * Powerful search\n  * Works on \n     most phones\n  * Supports custom \n     dictionaries");
+        aboutForm.addComponent(BorderLayout.CENTER, aboutLbl);
 
+        //Bottom part
+        Container lowerPart = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+        lowerPart.getStyle().setPadding(0, 0, 0, 0);
+        Label copyright1 = new Label("Original code by:");
+        Label copyright1a = new Label("    Ko Thar Thar (\u00A9 2009)");
+        Label copyright2 = new Label("Improvements by:");
+        Label copyright2a = new Label("    Seth N. Hetu (\u00A9 2009)");
+        copyright1.getStyle().setBgTransparency(0);
+        copyright1a.getStyle().setBgTransparency(0);
+        copyright1a.getStyle().setFgColor(0x0000FF);
+        copyright2.getStyle().setBgTransparency(0);
+        copyright2a.getStyle().setBgTransparency(0);
+        copyright2a.getStyle().setFgColor(0x0000FF);
+        copyright1.getStyle().setPadding(0, 0, 0, 0);
+        copyright1.getStyle().setMargin(0, 0, 0, 0);
+        copyright1a.getStyle().setPadding(0, 0, 0, 0);
+        copyright1a.getStyle().setMargin(0, 0, 0, 0);
+        copyright2.getStyle().setPadding(0, 0, 0, 0);
+        copyright2.getStyle().setMargin(5, 0, 0, 0);
+        copyright2a.getStyle().setPadding(0, 0, 0, 0);
+        copyright2a.getStyle().setMargin(0, 0, 0, 0);
+        lowerPart.addComponent(copyright1);
+        lowerPart.addComponent(copyright1a);
+        lowerPart.addComponent(copyright2);
+        lowerPart.addComponent(copyright2a);
+        aboutForm.addComponent(BorderLayout.SOUTH, lowerPart);
 
         //Commands and control (and transitions)
         okCommand = new Command("Ok");
@@ -65,6 +101,11 @@ public class AboutDialog {
             }
         });
         aboutForm.setTransitionOutAnimator(Transition3D.createCube(300, false));
+
+       //Match styles to the other forms, not to dialogs
+       aboutForm.setTitleStyle(MZMobileDictionary.GetHeaderStyle());
+       aboutForm.setMenuStyle(MZMobileDictionary.GetMenuStyle());
+       aboutForm.setStyle(MZMobileDictionary.GetBasicFormStyle());
     }
 
 }
